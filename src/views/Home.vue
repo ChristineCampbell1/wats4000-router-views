b9d6eb195a0cf0458fb49c0fe25076dd7e43aaa7
Christine Campbell <ccamp67@gmail.com>
2020-04-29 21:02:53 -0700

    a few lines of code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,54 @@
 <template>
   <div class="home">
-    <div class="form-container"><!-- TODO: Modulate display of the .form-container using v-show and the showForm variable. -->
+    <div class="form-container" v-show="showForm">
       <h1>Join the Web Developers Club!</h1>
       <p>Sign up to access our special, secret page. Just create an account and answer a brief survey.</p>
 
-      <!-- TODO: Add an HTML element to display an error message for when the user submits invalid information. Use v-show to show/hide this message based on the validity of the form data. -->
+      <div class="success-message" v-show="!showForm">
+        <form>
 
-      <form><!-- TODO: Add the proper v-on directive to let the validateForm method handle this form when it is submitted -->
+          <form v-on:submit.prevent="validateForm">
+            <!-- TODO: Add labels for each form field in this form. -->
 
-        <!-- TODO: Add labels for each form field in this form. -->
+            <!-- TODO: Add a username field. -->
 
-        <!-- TODO: Add a username field. -->
+            <!-- TODO: Add an email field. -->
 
-        <!-- TODO: Add an email field. -->
+            <!-- TODO: Add a password field. -->
 
-        <!-- TODO: Add a password field. -->
+            <!-- TODO: Add a passwordVerify field. -->
 
-        <!-- TODO: Add a passwordVerify field. -->
-
-
-        <p><input type="submit" value="Submit"></p>
-      </form>
+          <p>
+            <input type="submit" value="Submit" />
+          </p>
+          
+        </form>
+      </div>
     </div>
-    <div class="success-message"><!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
+    <div class="success-message">
+      <!-- TODO: Modulate display of the .success-message using v-show and the showForm variable. -->
       <h1>Thank you for signing up!</h1>
-      <p>Please take our new member survey. Click here</p><!-- TODO: Link "Click here" to the survey page. -->
+      <p>Please take our new member survey. Click here</p>
+      <!-- TODO: Link "Click here" to the survey page. -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-      username: '',
-      email: '',
-      password: '',
-      passwordVerify: '',
+      username: "",
+      email: "",
+      password: "",
+      passwordVerify: "",
       showForm: true,
       showError: false
-    }
+    };
   },
   methods: {
-    validateForm: function () {
+    validateForm: function() {
       // Validate the form by checking the following values:
       // username must not be blank
       // email must not be blank
@@ -51,10 +56,9 @@ export default {
       //
       // When the form is validated, show the .success-message content
       // If the form is invalid, show the form error message
-
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -64,7 +68,8 @@ export default {
   padding: 1rem;
   color: #aa0000;
 }
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
